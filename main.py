904d0f2ebbc384197cf60168269e937de14ca6e4
Victoriano Izquierdo <victoriano@gmail.com>
2023-04-26 18:12:55 -0400

    fixed metadata

--- main.py
+++ main.py
@@ -39,16 +39,30 @@ def download_photos(api, folder_path, num_photos):
         next_page_token = ""
         page_size = min(num_photos, 100)
         while num_photos > 0:
-            results = api.mediaItems().list(
-                pageSize=page_size,
-                pageToken=next_page_token
+            # Add the "filters" parameter to search for favorited items
+            results = api.mediaItems().search(
+                body={
+                    "pageSize": page_size,
+                    "pageToken": next_page_token,
+                    "filters": {
+                        "includeArchivedMedia": False,
+                        "featureFilter": {
+                            "includedFeatures": ["FAVORITES"]
+                        }
+                    }
+                }
             ).execute()
 
             items = results.get("mediaItems", [])
 
             for item in items:
+                # https://developers.google.com/photos/library/reference/rest/v1/mediaItems#MediaItem
                 print(f"Downloading {item['filename']}...")
-                response = requests.get(item["baseUrl"])
+
+                url = item["baseUrl"]
+                url += "=w1800"
+
+                response = requests.get(url)
                 image = Image.open(BytesIO(response.content))
                 file_ext = os.path.splitext(item["filename"])[1]
 
@@ -58,11 +72,11 @@ def download_photos(api, folder_path, num_photos):
                     print(f"Cannot save {item['filename']}: unsupported file format")
                     continue
 
-                if "metadata" in item:
+                if "mediaMetadata" in item:
                     exif_filename = os.path.splitext(item["filename"])[0] + "_exif.json"
                     exif_filepath = os.path.join(folder_path, exif_filename)
                     with open(exif_filepath, "w") as exif_file:
-                        json.dump(item["metadata"], exif_file, indent=2)
+                        json.dump(item["mediaMetadata"], exif_file, indent=2)
 
                 num_photos -= 1
                 if num_photos <= 0:
