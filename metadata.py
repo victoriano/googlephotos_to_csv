deceadeef00ff6f15a926bde05f7f7f4e8c02a3e
Victoriano Izquierdo <victoriano@gmail.com>
2023-05-14 21:53:05 +0200

    remove trailing s in the values of exposure time and add file type to csv

--- metadata.py
+++ metadata.py
@@ -7,15 +7,14 @@ def create_metadata_csv(folder_path, items, urls):
     with open(csv_filepath, "w", newline="") as csv_file:
         writer = csv.writer(csv_file)
 
-        # Add a "URL" column to the CSV header
-        writer.writerow(["Filename", "Creation Time", "Width", "Height", "Camera Make", "Camera Model", "Focal Length", "Aperture", "ISO Equivalent", "Exposure Time", "URL"])
+        writer.writerow(["Filename", "File Type", "Creation Time", "Width", "Height", "Camera Make", "Camera Model", "Focal Length", "Aperture", "ISO Equivalent", "Exposure Time", "Url"])
 
-        # Iterate through items and urls simultaneously
         for item, url in zip(items, urls):
             if "filename" not in item:
                 continue
 
             filename = item["filename"]
+            file_type = os.path.splitext(filename)[1][1:]  # Get file extension without dot
             metadata = item.get("mediaMetadata", {})
 
             creation_time = metadata.get("creationTime", "")
@@ -29,6 +28,7 @@ def create_metadata_csv(folder_path, items, urls):
             aperture_f_number = photo_metadata.get("apertureFNumber", "")
             iso_equivalent = photo_metadata.get("isoEquivalent", "")
             exposure_time = photo_metadata.get("exposureTime", "")
+            if exposure_time.endswith('s'):
+                exposure_time = exposure_time[:-1]
 
-            # Include the url in the row data
-            writer.writerow([filename, creation_time, width, height, camera_make, camera_model, focal_length, aperture_f_number, iso_equivalent, exposure_time, url])
+            writer.writerow([filename, file_type, creation_time, width, height, camera_make, camera_model, focal_length, aperture_f_number, iso_equivalent, exposure_time, url])
